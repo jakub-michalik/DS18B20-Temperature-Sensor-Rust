2bbef3314f7c998a787a6f77a4e55b08dee20cb5
jakub-michalik <77364412+jakub-michalik@users.noreply.github.com>
2023-10-12 00:30:05 +0200

    Create i2c.rs
    Mock GPIO and I2C modules.

---
+++ src/hal/i2c.rs
@@ -0,0 +1,12 @@
+// Mock GPIO and I2C modules
+pub struct GPIO {
+    // properties related to GPIO
+}
+
+// functions related to GPIO...
+
+pub struct I2C {
+    // properties related to I2C communication
+}
+
+// functions related to I2C communication...
